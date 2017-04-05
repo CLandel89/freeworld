365f130f09903b43243768597cc887cf7bcc4938
Titan L <titan1989@gmx.de>
2017-04-05 23:31:34 +0200

    Some creative exception handling in the main method.

--- src/integration/app/sp-sdl-sqlite/main.cpp
+++ src/integration/app/sp-sdl-sqlite/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 
 //use functions from the sp LIB
 #include "src/sp/sp.hpp"
@@ -9,8 +10,17 @@ int main(int argc, const char** args)
 	pkgs.push_back("std");
 	try {
 		Freeworld::start("asdf", pkgs, pkgs);
-	} catch (const std::string& err) {
-		std::cout << "Freeworld threw an exception:\n" << err << std::endl;
+	} catch (const std::string& e) {
+		std::cout << "Freeworld threw an exception:\n" << e << std::endl;
+	} catch (const int& e) {
+		std::cout << "Freeworld threw an exception: " << e << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << "Freeworld threw an exception:\n" << e.what() << std::endl;
+		throw e;
+	} catch (...) {
+		auto msg = "Freeworld threw an unhandled exception.";
+		std::cout << msg << std::endl;
+		throw msg;
 	}
 	Freeworld::stop();
 }
