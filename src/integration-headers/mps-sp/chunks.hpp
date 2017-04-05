d3ed2b445659061a17ff8e5356c909ef50825e87
Titan L <titan1989@gmx.de>
2017-04-05 21:34:04 +0200

    Cosmetic fix (source comment).

--- src/integration-headers/mps-sp/chunks.hpp
+++ src/integration-headers/mps-sp/chunks.hpp
@@ -11,7 +11,7 @@ typedef struct {
 void open_chunk(int32_t x, int32_t y, int32_t z);
 //the implementing end has to free the BinData
 //and its data component upon the next call
-//and to close it on the last, NULL returning call
+//and to close the db on the last, NULL returning call
 BinData* read_chunk();
 
 } } // end of namespace Freeworld::Integration
