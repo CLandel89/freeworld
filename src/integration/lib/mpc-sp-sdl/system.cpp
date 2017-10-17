046754a48695b991ead02b6a449b29f79cdee2cc
Titan L <titan1989@gmx.de>
2017-10-17 19:59:14 +0200

    Remove deprecated inclusion that mixes up SDL versions 1 and 2.

--- src/integration/lib/mpc-sp-sdl/system.cpp
+++ src/integration/lib/mpc-sp-sdl/system.cpp
@@ -1,5 +1,5 @@
 #include <SDL2/SDL.h>
-#include <SDL/SDL_image.h>
+#include <SDL2/SDL_image.h>
 
 //this source file implements the following interface header:
 #include "src/integration-headers/mpc-mps-sp/system.hpp"
